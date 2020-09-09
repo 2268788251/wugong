878f270cba82432b57d0a9fa651b68698b7fd394
zhang <2268788251@qq.com>
2020-09-09 18:43:27 +0800

    gaibian

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -5,7 +5,8 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("独孤九剑");
-
+		System.out.println("独孤九剑1111");
+		System.out.println("独孤九剑1111");
 	}
 
 }
